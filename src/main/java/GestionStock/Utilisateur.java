11b768849787161e3fd9e6bd6907b185e282b11a
Sidaty <sidatymohamedmahmud@gmail.com>
2025-02-07 23:54:28 +0000

    creation du classe utilisateur

---
+++ src/main/java/GestionStock/Utilisateur.java
@@ -0,0 +1,12 @@
+package GestionStock;
+
+public class Utilisateur {
+
+	
+	    private String id;
+	    private String email;
+	    private String role; // "ADMIN", "GESTIONNAIRE", "CLIENT"
+	    private String password;
+	}
+
+
