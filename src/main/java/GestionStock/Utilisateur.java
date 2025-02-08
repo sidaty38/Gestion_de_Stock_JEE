1154682b1ed9338d5ed9c6b48be143ab94d5b4aa
med moktar <medmoktar@gmail.com>
2025-02-08 23:07:59 +0200

    create class UtilisateurRepository

--- src/main/java/GestionStock/Utilisateur.java
+++ src/main/java/GestionStock/Utilisateur.java
@@ -1,11 +1,16 @@
 package GestionStock;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Utilisateur {
 
-	
+	    @JsonProperty
 	    private int id;
+	    @JsonProperty
 	    private String email;
-	    private String role; // "ADMIN", "GESTIONNAIRE", "CLIENT"
+	    @JsonProperty
+	    private String role;
+	    @JsonProperty// "ADMIN", "GESTIONNAIRE", "CLIENT"
 	    private String password;
 	    
 	    public Utilisateur(int id,String email,String role,String password) {
@@ -14,6 +19,39 @@ public class Utilisateur {
 	    	this.role=role;
 	    	this.password=password;
 	    	}
+	    public int getId() {
+	        return id;
+	    }
+
+	    public String getEmail() {
+	        return email;
+	    }
+
+	    public String getRole() {
+	        return role;
+	    }
+
+	    public String getPassword() {
+	        return password;
+	    }
+
+	    
+
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+
+	    public void setRole(String role) {
+	        this.role = role;
+	    }
+
+	    public void setPassword(String password) {
+	        this.password = password;
+	    }
+
+	    public String toString() {
+	        return "Utilisateur{id=" + id + ", Email=" + email + ", Role=" + role + ",Password="+ password +" }";
+	    }
 	}
 
 
---
+++ src/main/java/GestionStock/UtilisateurRepository.java
@@ -0,0 +1,36 @@
+package GestionStock;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UtilisateurRepository {
+	
+	static Map<Integer,Utilisateur> users=new HashMap<>();
+	
+	static {
+		users.put(1, new Utilisateur(1, "admin", "Admin", "admin"));
+		users.put(2, new Utilisateur(2,"gestionnaire","gestionnaire","1234"));
+	}
+	
+	public Map<Integer,Utilisateur> get() {
+		return users;
+	}
+	public Utilisateur getbyid(int id){
+		return users.get(id);
+	}
+	
+	public void save(Utilisateur user) {
+		int id=user.getId();
+		users.put(id,user);
+	}
+	
+	public void deletebyid(int id) {
+		users.remove(id);
+	}
+	
+	public Utilisateur update(int id,Utilisateur user) {
+		users.replace(id, user);
+		return user;
+	}
+
+}
