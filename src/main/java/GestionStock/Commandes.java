c1374ed33b5ea162fe89a7f54b4882ff01c946ce
Heddi Oussame <XPRISTO@DESKTOP-9MJG61C.iscae.mr>
2025-03-01 10:40:14 +0100

    les getters et les setters

--- src/main/java/GestionStock/Commandes.java
+++ src/main/java/GestionStock/Commandes.java
@@ -14,5 +14,36 @@ public class Commandes {
 		@JsonProperty
 		private Map<Integer, Integer> produits;
 		private String dateCommande;
+		
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getStatus() {
+			return status;
+		}
+		public void setStatus(String status) {
+			this.status = status;
+		}
+		public double getPrix() {
+			return prix;
+		}
+		public void setPrix(double prix) {
+			this.prix = prix;
+		}
+		public Map<Integer, Integer> getProduits() {
+			return produits;
+		}
+		public void setProduits(Map<Integer, Integer> produits) {
+			this.produits = produits;
+		}
+		public String getDateCommande() {
+			return dateCommande;
+		}
+		public void setDateCommande(String dateCommande) {
+			this.dateCommande = dateCommande;
+		}
 
 }
