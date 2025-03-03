fd578ee40cab2eff86ab4e8157458f3bc5f00e04
Mohammed Beimine <i19532.etu@iscae.mr>
2025-03-03 02:39:22 +0000

    Methode Save Created For Saving Commands

--- src/main/java/GestionStock/CommandeRepository.java
+++ src/main/java/GestionStock/CommandeRepository.java
@@ -5,5 +5,12 @@ import java.util.Map;
 
 public class CommandeRepository {
 	static Map<Integer,Commandes> commande=new HashMap<>();
+	
+	public Map<Integer,Commandes> get() {
+		return commande;
+	}
+	public void save(Commandes c) {
+		int id=c.getId();
+		commande.put(id, c);
 
-}
+}}
